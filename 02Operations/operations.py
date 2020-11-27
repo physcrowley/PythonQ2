f3d0b28aa75fe4256164cbe510387145b3be0aa5
physcrowley <crowlda@ecolecatholique.ca>
2020-11-27 13:32:17 -0500

    added comments and image to operations.py

--- 02Operations/operations.py
+++ 02Operations/operations.py
@@ -2,18 +2,48 @@
 # Par : David Crowley, crowlda@ecolecatholique.ca
 # 2020/11/27
 
+#-----------TESTER DES OPÉRATIONS--------------#
+#
+# C'est pratique de tester des choses avant de
+# les inclure directement dans notre programme. C'est
+# possible grâce au shell python --> un mode d'utiliser
+# python directement à la console.
+# 
+# Normalement, on passe un fichier (notre programme) à
+# Python avec la commande à la console :
+# 
+# python <fichier>.py
+# 
+# Pour travailler directement avec Python sans fichier,
+# il faut simplement écrire :
+# 
+# python
+# 
+# L'apparence de la console change et tu peux entrer des 
+# déclarations Python un-par-un pour les tester.
+# 
+# Pour quitter le mode shell de python, tu passes la 
+# commande :
+# 
+# quit()
+#
+
+
 #---------OPERATIONS MATHÉMATIQUES-------------#
 #
 # +, -, /, *, module math
+#
 
 
 #------------COMPARAISONS-------------#
 #
 # ==, >, <, >=, <=, !=
+#
 
 
 
 #----------MANIPULATION DE TEXTE----------#
 #
-# lower(), upper()
+# lower(), upper() --> pour changer la case
+# ord(), chr() --> pour des caractères individuels
 #
