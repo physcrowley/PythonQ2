45f764dd686d37bbeeaa52393fce741fad4bff5b
physcrowley <crowlda@ecolecatholique.ca>
2020-11-27 10:05:18 -0500

    initier le fichier operations.py

---
+++ 02Operations/operations.py
@@ -0,0 +1,19 @@
+# Exemples des opérations de base en Python
+# Par : David Crowley, crowlda@ecolecatholique.ca
+# 2020/11/27
+
+#---------OPERATIONS MATHÉMATIQUES-------------#
+#
+# +, -, /, *, module math
+
+
+#------------COMPARAISONS-------------#
+#
+# ==, >, <, >=, <=, !=
+
+
+
+#----------MANIPULATION DE TEXTE----------#
+#
+# lower(), upper()
+#
