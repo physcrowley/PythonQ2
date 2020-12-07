c840ce5936ce226188b24cd792650d031ca32e62
physcrowley <crowlda@ecolecatholique.ca>
2020-12-07 14:48:23 -0500

    exemples de conditions

--- 03Conditions/booleans.py
+++ 03Conditions/booleans.py
@@ -19,19 +19,54 @@
 # pour nous rappeller que la valeur est soit True ou False
 
 
-# un exemple
-estBon = False
-print(type(estBon))
+# EXEMPLE où le booléen remplace une expression de comparaison
+# Le code ci-dessous change la valeur de isOn entre True et False.
+# S'il était False au début, il finit par être True et vice-versa.
+# C'est comme le comportement d'un bouton sur un appareil.
+isOn = False
+print(type(isOn)) # <- 'bool' ou booléen
 
-if estBon:
-    print("bravo, on continue")
+if isOn: # <- sera vrai si isOn = True
+    isOn = False
+    print("éteint")
+else: # <- si isOn = False
+    isOn = True
+    print("allumé")
+
+print(isOn)
+
+
+# EXEMPLE où le booléen est utilisé comme indicateur pour nous
+# permettre d'agir plus tard dans le programme au lieu de directement
+# à l'intérieur de la séquence de conditions
+estOui = False 
+print("oui ou non")
+choix = input().lower()
+
+# Au lieu de faire quelque chose directement, on change juste
+# la valeur de notre indicateur estOui. Cela nous permet de
+# développer de la logique plus complexe (ici, demander une
+# confirmation en cas de la réponse "non").
+if choix == "oui":
+    estOui = True
+    print("oui...")
+elif choix == "non":
+    print("non...")
+    print("confirme ton choix, SVP. Tu veux oui ou non?")
+    choix = input().lower()
+
+    if choix == "oui":
+        estOui = True
+        print("oui...")
+    else:
+        print("non... c'est clair.")
+else: # capter l'erreur et le présenter à l'utilisateur
+    print("je n'ai pas compris la réponse. je présume non")
+
+# Maintenant, on va agir sur la réponse en utilisant la
+# valeur de notre indicateur estOui (qui est soit True ou False)
+if estOui:
+    print("maintenant on joue!")
 else:
-    estBon = True
-    print("maintenant on est bon")
+    print("Au revoir!")
 
-print(estBon)
-
-
-# try-except -> structure comme if-else mais ou le code dans le
-# try peut faire planter le programme
-# * les conversions int() et float() par exemple
---
+++ 03Conditions/try_except.py
@@ -0,0 +1,3 @@
+# try-except -> structure comme if-else mais ou le code dans le
+# try peut faire planter le programme
+# * les conversions int() et float() par exemple
