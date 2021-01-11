c15a57c3c84c9c5c67fa14926a1411e7f157c8fd
physcrowley <crowlda@ecolecatholique.ca>
2021-01-11 14:12:56 -0500

    added animation example

--- 06GUI/proj_animation/anim.py
+++ 06GUI/proj_animation/anim.py
@@ -1,9 +1,23 @@
 import pgzrun
 
-WIDTH  = 800
-HEIGH = 600
+WIDTH = 800
+HEIGHT = 200
+
+# Sprite
+alien = Actor("alien.png")
+alien.pos = (0, HEIGHT / 2)
+speed = 1
+
 
 def draw():
+    """ Tout ce qu'on veut voir dans notre fenêtre"""
     screen.fill("blue")
+    alien.draw()
+
+def update():
+    """Changer des choses entre chaque mise à jour de la fenêtre"""
+    alien.x = alien.x + speed
 
 pgzrun.go()
+# go() lance le programmme Pygame Zero qui va appeler les 
+# fonctions qu'on a définit ici, comme draw() et update()
